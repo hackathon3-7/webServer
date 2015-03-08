68f223e8d41bfa2c530f92cb765fb6928840d356
Tony <jinniw43805@gmail.com>
2015-03-08 15:05:28 +0800

    add css

--- search_video.php
+++ search_video.php
@@ -104,6 +104,62 @@ foreach ($cameraData as $cam) {
   
 }
 ?>
-<video width="480" height="320" controls="controls">
-<source src="http://<?php echo $SERVER_IP; ?>/webServer/hpc_portal_demo1.1.mp4" type="video/mp4">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Search</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="package/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="stylesheet/boot.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+
+            <div class="inner">
+              <h3 class="masthead-brand">Cover</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="#">Features</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <video width="480" height="320" controls="controls">
+            <source src="http://<?php echo $SERVER_IP; ?>/webServer/hpc_portal_demo1.1.mp4" type="video/mp4">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            <script src="/package/bootstrap/dist/js/bootstrap.min.js"></script>
+            <script src="../../assets/js/docs.min.js"></script>
+            <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+            <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+              </div> 
+        
+        </div>
+      </div>
+    </div>
+  </body>
+
 </video>
+</html>
