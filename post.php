05b78d3a9617ab8bf8491d9a91c18214af27e998
Tony <jinniw43805@gmail.com>
2015-03-08 11:39:53 +0800

    Add Epoch Time

--- post.php
+++ post.php
@@ -1,10 +1,16 @@
 <?php
+
+	//One day = 86400 seconds, server can + - this area to decide
+	
 	$inLat=$_POST["inLat"];
 	$inLng=$_POST["inLng"];
-	$date=$_POST["date"];
+	$date=$_POST["epochDate"];
+	$time=$_POST["epoch"];
 	echo "Lat:".$inLat;
 	echo "<br>";
 	echo "Lng".$inLng;
 	echo "<br>";
-	echo "Date:".$date;	
+	echo "Epoch,Date:".$date;	
+	echo "<br>";
+	echo "Time:".$time;
 ?>
