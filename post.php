ba623fda2b344406c27d5c8b73863a41fc6326d8
Tony <jinniw43805@gmail.com>
2015-03-08 10:39:29 +0800

    Add date , jquery and css

--- post.php
+++ post.php
@@ -1,8 +1,10 @@
 <?php
 	$inLat=$_POST["inLat"];
 	$inLng=$_POST["inLng"];
+	$date=$_POST["date"];
 	echo "Lat:".$inLat;
 	echo "<br>";
 	echo "Lng".$inLng;
-	
+	echo "<br>";
+	echo "Date:".$date;	
 ?>
